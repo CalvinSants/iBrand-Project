50eade8c420eadf779ca5d76fa37ac17ae946c1b
Calvin <calvinsantanadossantos@gmail.com>
2021-02-09 22:41:51 -0300

    second commit

---
+++ verification.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Sistema</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+<body>
+    <form method="post" action="verifying.php">
+        <p>Digite a senha:</p>
+        <input type="password" name="senha"><br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
